ad7def18e27f2a131f4df36906261f098991de62
fgitonga <mfredgitonga@gmail.com>
2024-08-02 21:18:25 +0300

    refact: reduce code in main created functions to call in main

--- main.go
+++ main.go
@@ -1,11 +1,9 @@
 package main
 
 import (
-	"flag"
+	"ascii-art-color/src"
 	"fmt"
 	"os"
-	"regexp"
-	"ascii-art-color/src"
 )
 
 func main() {
@@ -13,63 +11,24 @@ func main() {
 	if len(os.Args) < 4 {
 		fmt.Println("Usage: go run . [OPTION] [STRING]")
 		fmt.Println("EX: go run . --color=<color> <letters to be colored> \"something\"")
-
-	} else {
-		// Define a flag named "color"
-
-		colorPtr := flag.String("color", "something", "letters to be colored")
-
-		// Parse the command-line flags
-		flag.Parse()
-
-		// Access values of the flags
-
-		colorName := *colorPtr
-
-		// obtaining user input
-		lettersToColor := os.Args[2]
-		words := os.Args[3]
-		bannerFileName := "banners/standard.txt"
-
-		// obtainng RGB values for color using 'PrintColored' function
-		r, g, b := src.PrintColored(colorName)
-
-		// Concatenate everything into a string
-		foregroundColor := fmt.Sprintf("\x1b[38;2;%d;%d;%dm", r, g, b)
-		reset_color := "\x1b[0m"
-
-		// Regular expression pattern to match the word
-		re := regexp.MustCompile(regexp.QuoteMeta(lettersToColor))
-
-		// Find all matches
-		words = src.ReadInput(words) // + "\n"
-		// fmt.Println(words)
-		matches := re.FindAllStringSubmatchIndex(words, -1)
-
-
-		if len(matches) == 0 {
-			fmt.Print(src.PrintAsciiArt(words, bannerFileName))
-			// fmt.Printf("%s", result)
-		} else {
-			lastIndex := 0
-
-			for _, match := range matches {
-				start, end := match[0], match[1]
-
-				if start > lastIndex {
-					fmt.Print(src.PrintAsciiArt(words[lastIndex:start], bannerFileName))
-				}
-
-			 
-				fmt.Print(foregroundColor,src.PrintAsciiArt(words[start:end], bannerFileName),reset_color)
-			
-
-				lastIndex = end
-			}
-			if lastIndex < len(words) {
-				fmt.Print(src.PrintAsciiArt(words[lastIndex:], bannerFileName))
-			}
-		}
-
+		return
 	}
+
+	inputStr, subStr,color, bannerName, err := src.HandleArgs()
+	
+
+	bannerContent,err := src.GetBanner(bannerName)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	asciiMap := src.ReadBannerContent(bannerContent)
+	words, err  := src.ReadInput(inputStr) // + "\n"
+	if err != nil {
+		println(err)
+		return
+	}
+
+	src.PrintAsciiArt(words, subStr, color, asciiMap)
+	
 }
