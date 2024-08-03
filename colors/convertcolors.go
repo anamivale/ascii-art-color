cd52fedbe4dbf62b200f31ca18dff747352ea46f
fgitonga <mfredgitonga@gmail.com>
2024-08-03 21:08:36 +0300

    refact: update color hex to rgb conversion in colorise func

--- colors/convertcolors.go
+++ colors/convertcolors.go
@@ -73,8 +73,20 @@ func PrintColored(color string) (int, int, int, error) {
 		"palevioletred":    [3]int{219, 112, 147},
 		"mediumvioletred":  [3]int{199, 21, 133},
 	}
-
-	r, g, b := 0, 0, 0
+	r,g,b := 0,0,0
+	if strings.HasPrefix(color,"rgb"){
+		r,g,b, err :=  RgbTo256ColorCode(color)
+		if err != nil{
+			return 0,0,0,err
+		}
+		return r,g,b ,nil
+	} else if strings.HasPrefix(color,"#"){
+		r,g,b, err :=  HexTo256(color)
+		if err != nil{
+			return 0,0,0,err
+		}
+		return r,g,b ,nil
+	} else {
 	if value, ok := colors[color]; ok {
 		r = value[0]
 		g = value[1]
@@ -82,47 +94,41 @@ func PrintColored(color string) (int, int, int, error) {
 	} else {
 		return 0, 0, 0, fmt.Errorf(" Error: %v is not available",color)
 	}
-
+	} 
 	return r, g, b, nil
-}
 
-// RgbTo256 converts RGB values to a 256-color code value
-// based on the 6x6x6 color cube used in trminal color schemes.
-func RgbTo256(r, g, b int) int {
-	var rI, gI, bI int = r / 51, g / 51, b / 51 // Reduce each color component to fit in 6 levels (0-5).
-	return 16 + 36*rI + 6*gI + bI               // Claculate 256-color code.
 }
 
 // HexTo256 converts a hex color string (e.g., "FFA500") to a 256-color code.
 // Returns an error if the input format is incorrect.
-func HexTo256(hex string) (int, error) {
-	hex = strings.TrimPrefix(hex, "H")
+func HexTo256(hex string) (int,int,int, error) {
+	hex = strings.TrimPrefix(hex, "#")
 	if len(hex) != 6 {
-		return 0, fmt.Errorf("invalid Hex color code lenth: %v", hex)
+		return 0,0,0, fmt.Errorf("invalid Hex color code lenth: %v", hex)
 	}
 
 	var r, g, b int
 	_, err := fmt.Sscanf(hex, "%02x%02x%02x", &r, &g, &b) // Parse hex into RGB values.
 	if err != nil {
-		return 0, fmt.Errorf("invalid Hex color code: %v", hex)
+		return 0,0,0, fmt.Errorf("invalid Hex color code: %v", hex)
 	}
-	return RgbTo256(r, g, b), nil // Convert RGB to 256-color code.
+	return r, g, b, nil // Convert RGB to 256-color code.
 }
 
 // RgbTo256ColorCode converts a string representation of an RGB color
 // (e.g., "rgb(255,165,0)") to a 256-color code.
 // Returns an error if the input format is incorrect.
-func RgbTo256ColorCode(rgb string) (int, error) {
+func RgbTo256ColorCode(rgb string) (int,int,int, error) {
 	rgb = strings.TrimPrefix(rgb, "rgb(")
 	rgb = strings.TrimSuffix(rgb, ")")
 
 	var r, g, b int
 	_, err := fmt.Sscanf(rgb, "%d,%d,%d", &r, &g, &b) // Parse RGB values.
 	if err != nil {
-		return 0, fmt.Errorf("invalid RGB color code: format %v", rgb)
+		return 0,0,0, fmt.Errorf("invalid RGB color code: format %v", rgb)
 	}
 	if r < 0 || r > 255 || g < 0 || g > 255 || b < 0 || b > 255 {
-		return 0, fmt.Errorf("RGB values must be in the range 0-255: %v", rgb)
+		return 0,0,0, fmt.Errorf("RGB values must be in the range 0-255: %v", rgb)
 	}
-	return RgbTo256(r, g, b), nil
+	return r, g, b, nil
 }
